32bf0e743a86fa6554baad182536c2e3eda37fae
Ruta tewelde <rutaty26@gmail.com>
2024-05-15 22:27:39 -0400

    Update

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import "./header.css";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import NetflixLogo from "../../assets/Images/NetflixLogo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else setShow(false);
+    });
+  }, []);
+  return (
+    <div className={`header_outer_container ${show && "nav_black"}`}>
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix logo " width="100" />
+            </li>
+
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />{" "}
+            </li>
+            <li>
+              <NotificationsNoneIcon />{" "}
+            </li>
+            <li>
+              <AccountBoxIcon />{" "}
+            </li>
+            <li>
+              <ArrowDropDownIcon />{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header
